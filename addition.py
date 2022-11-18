4ecb2c628df218e612965c81770d11b9338038cf
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-11-18 09:07:12 -0800

    simple gui interface

--- addition.py
+++ addition.py
@@ -1,8 +1,37 @@
 import random
 import tkinter
 
-problems = [(x, y) for x in range(1, 11) for y in range(1,11)]
-selected_problems = random.sample(problems, 50)
+NUM_PROBLEMS = 50
+
+problems = [(x, y) for x in range(1, 11) for y in range(1, 11)]
+selected_problems = random.sample(problems, NUM_PROBLEMS)
 incorrect = []
+correct = []
 
 
+root = tkinter.Tk()
+
+remaining = tkinter.Label(root, text=f"{len(selected_problems)} problems left")
+remaining.pack()
+
+correct_count = tkinter.Label(root, text=f"Correct: {len(correct)}")
+correct_count.pack()
+
+incorrect_count = tkinter.Label(root, text=f"Incorrect: {len(incorrect)}")
+incorrect_count.pack()
+
+result = tkinter.Label(root, text=" ")
+result.pack()
+
+problem = tkinter.Label(
+    root, text=f"{selected_problems[0][0]} + {selected_problems[0][1]}"
+)
+problem.pack()
+
+entry_window = tkinter.Entry(root)
+entry_window.pack()
+
+submit_button = tkinter.Button(root, text="Submit")
+submit_button.pack()
+
+tkinter.mainloop()
